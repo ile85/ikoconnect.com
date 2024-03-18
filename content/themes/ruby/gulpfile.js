87d0305c4deb413f15b49e55d952969e9e6bcf76
ile85 <ilco_dimeski@live.co.uk>
2024-03-18 00:39:35 +0000

    final with gh.

--- content/themes/ruby/gulpfile.js
+++ content/themes/ruby/gulpfile.js
@@ -1,15 +1,7 @@
 const {series, parallel, watch, src, dest} = require('gulp');
 const pump = require('pump');
-
-// gulp plugins and utils
 const livereload = require('gulp-livereload');
 const postcss = require('gulp-postcss');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const beeper = require('beeper');
-const zip = require('gulp-zip');
-
-// postcss plugins
 const easyimport = require('postcss-easy-import');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
@@ -22,11 +14,11 @@ function serve(done) {
 function handleError(done) {
     return function (err) {
         if (err) {
-            beeper();
+            console.error(err);
         }
         return done(err);
     };
-};
+}
 
 function hbs(done) {
     pump([
@@ -48,42 +40,10 @@ function css(done) {
     ], handleError(done));
 }
 
-function js(done) {
-    pump([
-        src([
-            'node_modules/@tryghost/shared-theme-assets/assets/js/v1/lib/**/*.js',
-            'node_modules/@tryghost/shared-theme-assets/assets/js/v1/main.js',
-            'assets/js/lib/*.js',
-            'assets/js/main.js'
-        ], {sourcemaps: true}),
-        concat('main.min.js'),
-        uglify(),
-        dest('assets/built/', {sourcemaps: '.'}),
-        livereload()
-    ], handleError(done));
-}
-
-function zipper(done) {
-    const filename = require('./package.json').name + '.zip';
-
-    pump([
-        src([
-            '**',
-            '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**',
-            '!yarn-error.log'
-        ]),
-        zip(filename),
-        dest('dist/')
-    ], handleError(done));
-}
-
 const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs'], hbs);
 const cssWatcher = () => watch('assets/css/**/*.css', css);
-const jsWatcher = () => watch('assets/js/**/*.js', js);
-const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher);
-const build = series(css, js);
+const watcher = parallel(hbsWatcher, cssWatcher);
+const build = series(css);
 
 exports.build = build;
-exports.zip = series(build, zipper);
 exports.default = series(build, serve, watcher);
